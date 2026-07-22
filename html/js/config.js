e8eb51748ce6a96cea2cb2fab60d4554b0026748
boubker cheyoukh <nikaimkjf289@gmail.com>
2026-07-22 16:50:11 +0100

    Update config

--- html/js/config.js
+++ html/js/config.js
@@ -6,6 +6,8 @@ const API_URL = "https://copilot-rag-api-production-7ecd.up.railway.app";
 // const API_URL = "http://localhost:8000";
 
 // Supabase Configuration
-const SUPABASE_URL = "https://ffkvljqalgedexxmxtuh.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZma3ZsanFhbGdlZGV4eG14dHVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0MDA3NDIsImV4cCI6MjA5NDk3Njc0Mn0.zVo_fAWWgzgUICgqWsHhjVHqnTQTERUeAPKBz7s_MJA";
+//const SUPABASE_URL = "https://ffkvljqalgedexxmxtuh.supabase.co";
+//const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZma3ZsanFhbGdlZGV4eG14dHVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0MDA3NDIsImV4cCI6MjA5NDk3Njc0Mn0.zVo_fAWWgzgUICgqWsHhjVHqnTQTERUeAPKBz7s_MJA";
 
+const SUPABASE_URL = "https://kxwypxxypgbsazaxgqcv.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4d3lweHh5cGdic2F6YXhncWN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ3MjkyNTQsImV4cCI6MjEwMDMwNTI1NH0.D24i2ukdBZ_zFRY1dOVkKLkBGSZ8ndWBSQyufiBosi8";
